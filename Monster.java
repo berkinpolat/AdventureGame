0ee94a928506c34385a260260a211340a19d74bb
Berkin Polat <48439107+berkinpolat@users.noreply.github.com>
2022-02-22 22:46:39 +0300

    Monster Class Initialization

--- Monster.java
+++ Monster.java
@@ -1,2 +1,55 @@
 public class Monster {
+    private int id;
+    private String name;
+    private int damage;
+    private int health;
+    private int loot;
+
+    public Monster(int id, String name, int damage, int health, int loot) {
+        this.id = id;
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.loot = loot;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLoot() {
+        return loot;
+    }
+
+    public void setLoot(int loot) {
+        this.loot = loot;
+    }
 }
--- Zombie.java
+++ Zombie.java
@@ -1,2 +1,7 @@
 public class Zombie extends Monster{
+    public Zombie() {
+        super(1, "Zombie", 3, 10, 4);
+    }
+
+
 }
